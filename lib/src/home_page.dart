cf12044fa2e2ef164c983d294d6d0c9bed587f3c
Francisco Ortega <srfrancisco17@gmail.com>
2021-10-23 19:17:47 -0500

    Se completa el #67

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,14 +18,31 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    menuProvider.cargarData();
+    // menuProvider.cargarData();
 
-    return ListView(
-      children: _listaItems(),
-    );
+    return FutureBuilder(
+        future: menuProvider.cargarData(),
+        // initialData: [],
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+          return ListView(
+            children: _listaItems(snapshot.data),
+          );
+        });
   }
 
-  List<Widget> _listaItems() {
-    return [ListTile(title: Text('Hola Mundo'))];
+  List<Widget> _listaItems(List<dynamic>? data) {
+    final List<Widget> opciones = [];
+
+    data?.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones.add(widgetTemp);
+    });
+
+    return opciones;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
 
 class _MenuProvider {
@@ -7,9 +9,14 @@ class _MenuProvider {
     cargarData();
   }
 
-  cargarData() {
-    rootBundle.loadString('data/menu_opts.json').then((data) => print(data));
-  }
+  Future<List<dynamic>> cargarData() async =>
+      rootBundle.loadString('data/menu_opts.json').then((data) {
+        Map dataMap = json.decode(data);
+        opciones = dataMap['rutas'];
+        print(dataMap);
+
+        return opciones;
+      });
 }
 
 final menuProvider = _MenuProvider();
