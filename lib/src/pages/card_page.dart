d05e7ee28a7d5361b3f3a9c79850de179cfeb46b
Francisco Ortega <srfrancisco17@gmail.com>
2021-10-25 17:04:32 -0500

    Cards

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text('Titulo de la tarjeta'),
+            subtitle: Text('Subtitulo de la tarjeta'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(onPressed: () {}, child: Text('Cancelar')),
+              TextButton(onPressed: () {}, child: Text('Aceptar')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
